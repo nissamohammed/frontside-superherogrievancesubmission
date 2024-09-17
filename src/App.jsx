84cd977cd48e6ced0a0cb0031deb4073a065898b
nissamohammed <nissamohammed8@gmail.com>
2024-09-17 22:58:36 +0530

    server url changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom"
 import Landingpage from "./components/Landingpage"
-import Pagenotfound from "./pages/Pagenotfound"
-import Auth from "./pages/Auth"
 import Userdashboard from "./pages/Userdashboard"
-import Admindashboard from "./pages/Admindashboard"
 import About from './pages/About'
-import Complaintdiagram from './pages/Complaintdiagram'
+
 
 function App() {
 
@@ -16,12 +13,8 @@ function App() {
     <>
       <Routes>
          <Route path='/' element={<Landingpage/>}/>
-         <Route path='/login' element={<Auth/>}/>
          <Route path='/about' element={<About/>}/>
          <Route path='/userdashboard' element={<Userdashboard/>}/>
-         <Route path='/admindashboard' element={<Admindashboard/>}/>
-         <Route path='/chart' element={<Complaintdiagram/>}/>
-         <Route path='*' elements={<Pagenotfound/>}/>
       </Routes>
     </>
   )
